d652b5c901836391f58079f8ec8182972a785b2b
Damien Schneider <damien.schneider01@gmail.com>
2023-01-22 01:46:55 +0100

    Modify Navbar with Icons and conditions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,37 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 // styles & images
 import './Navbar.css'
-import Temple from '../assets/temple.svg'
+import AppIcon from '../assets/app_icon.svg'
 
 export default function Navbar() {
-    return (
-        <nav className="navbar">
-            <ul>
-                <li className="logo">
-                    <img src={Temple} alt="dojo logo" />
-                    <span>Poly Manage</span>
-                </li>
+  const { logout, isPending } = useLogout()
+  const { user } = useAuthContext()
 
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/signup">Signup</Link></li>
-                <li>
-                    <button className="btn">Logout</button>
-                </li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className="navbar">
+      <ul>
+        <li className="logo">
+          <img src={AppIcon} alt="dojo logo" />
+          <span>Poly Management</span>
+        </li>
+
+        {!user && (
+          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+          </>
+        )}
+
+        {user && (
+          <li>
+            {!isPending && <button className="btn" onClick={logout}>Logout</button>}
+            {isPending && <button className="btn" disabled>Logging out...</button>}
+          </li>
+        )}
+      </ul>
+    </nav>
+  )
 }
