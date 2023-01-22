851cb0844a4259009f673b6a0abb66d1f3840177
Damien Schneider <damien.schneider01@gmail.com>
2023-01-22 01:49:14 +0100

    See online users and update sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,40 +1,41 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom"
+import { useAuthContext } from '../hooks/useAuthContext'
 
-//styles & images
+// components
+import Avatar from "./Avatar"
 
-import './Sidebar.css'
-import DashboardIcon from '../assets/dashboard_icon.svg';
-import AddIcon from '../assets/add_icon.svg';
-
-
-
+// styles & images
+import "./Sidebar.css"
+import DashboardIcon from '../assets/dashboard_icon.svg'
+import AddIcon from '../assets/add_icon.svg'
 
 export default function Sidebar() {
-    return (
-        <div className='sidebar'>
-            <div className='sidebar-content'>
-                <div className='user'>
-                    {/* Avatar and username later */}
-                    <p>Hey user</p>
-                </div>
-                <nav className='links'>
-                    <ul>
-                        <li>
-                            <NavLink to="/">
-                                <img src={DashboardIcon} alt="add project icon" />
-                                <span>Dashboard</span>
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/">
-                                <img src={AddIcon} alt="add project icon" />
-                                <span>New Project</span>
-                            </NavLink>
-                        </li>
+  const { user } = useAuthContext()
 
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    )
+  return (
+    <div className="sidebar">
+      <div className="sidebar-content">
+        <div className="user">
+          <Avatar src={user.photoURL} />
+          <p>Hey {user.displayName}</p>  
+        </div>  
+        <nav className="links">
+          <ul>
+            <li>
+              <NavLink exact to="/">
+                <img src={DashboardIcon} alt="dashboard icon" />
+                <span>Dashboard</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/create">
+                <img src={AddIcon} alt="add project icon" />
+                <span>New Project</span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  )
 }
